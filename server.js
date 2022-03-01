781bd5c2342347f55012b48abf4ec8165c6c8c4e
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-02-28 21:36:27 -0600

    connect to the MySQL database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +12,19 @@ app.use(express.json());
 app.use((req, res) => {
     res.status(404).end();
 });
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'PotBuster132!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
