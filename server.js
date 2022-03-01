90fdbddd3be5ad52e9b90cd71ef1d2638d4439aa
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-02-28 21:57:08 -0600

    execute callback function

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const mysql = require('mysql2');
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +7,6 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded ({ extended: false }));
 app.use(express.json());
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
 
 // Connect to database
 const db = mysql.createConnection(
@@ -25,6 +20,15 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
 );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
